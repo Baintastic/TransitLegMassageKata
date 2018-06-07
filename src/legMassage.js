2eb24e14ff386d69a5ca3a46589ca26a7910a071
Thabani Tembe <ThabaniT@SAHOMELOANS.COM>
2018-06-07 16:27:42 +0200

    wip with refractoring

--- src/legMassage.js
+++ src/legMassage.js
@@ -1,77 +1,45 @@
 function legMassage() {
     fastestRunner = function (yd, ys, t1d, t1s, t2d, t2s, broken) {
-        let time1 = yd / ys;
-        let time2 = t1d / t1s;
-        let time3 = t2d / t2s;
-        let time = [time1, time2, time3];
+        
+        let getTimes = function(){
+            let time1 = yd / ys;
+            let time2 = t1d / t1s;
+            let time3 = t2d / t2s;
+            return [time1, time2, time3]
+        }
+
+        let FindFastestRunner = function(index){
+            if (times[index] <= fastestTime) {
+                fastestTime = times[index];
+                firstRunner = tourists[index];
+            }     
+        }
+        let times = getTimes();
         let tourists = ["y", "t1", "t2"];
-        //console.log(time1);
-        //console.log(time2);
         let firstRunner = "";
-        let fastestTime = time[0];
-        let distanceBetweenMachines = 2;
-        let reactionTime = 3;
-        //console.log("tour is " + tourists);
-       // console.log("time is " + time);
+        let fastestTime = times[0];
+
         for (let i = 0; i <= tourists.length; i++) {
-            //console.log("time is " + time[i]);
-            if (time[i]==="undefined"|| isNaN(time[i]) || time[i]===0 ) {
-                time.splice(i, 1);
+            if (isNaN(times[i])) {
+                times.splice(i, 1);
                 tourists.splice(i, 1);
-                //console.log(time);
-                //console.log("first timelist is " + time);
             }
             else {
-                if (time[i] <= fastestTime) {
-                    fastestTime = time[i];
-                    firstRunner = tourists[i];
-                    //console.log("second timelist is" + time);
-                }
+                FindFastestRunner(i);
             }
-           
-            
         }
-        console.log("first runner is " + firstRunner);
+
         if (broken === true) {
-            fastestTime += reactionTime + distanceBetweenMachines;
+            let reactionTime = 5;
+            fastestTime += reactionTime;
             fastestTouristIndex = tourists.indexOf(firstRunner);
-            time[fastestTouristIndex] = fastestTime;
-            console.log("first runner is " + fastestTime);
-
+            times[fastestTouristIndex] = fastestTime;
             for (let j = 0; j <= tourists.length; j++) {
-                
-                console.log("first person is " + time[j]);
-                if (time[j] <= fastestTime) {
-                    fastestTime = time[j];
-                    firstRunner = tourists[j];
-                    console.log("fastest runer is " + firstRunner);
-                      console.log("third timelist is " + time);
-                }
-
+                FindFastestRunner(j);
             }
-           
         }
-        
-
-        // if (broken === true) {
-        //     fastestTime += reactionTime + distanceBetweenMachines;
-
-        //     for(let j=0;j<tourists.length;j++){
-        //         if (time[j] < fastestTime) {
-                   
-        //             fastestTime = time[j];
-        //             firstRunner = tourists[j];
-        //             console.log("fastest runer is " + firstRunner);
-        //             console.log("first runner is " + time);
-        //         }
-        //     }
-           
-
-        //     }
-            return firstRunner;
-        }
-       
-    
+        return firstRunner;
+    }
 
     return {
         fastestRunner: fastestRunner
--- test/legMassageTests.spec.js
+++ test/legMassageTests.spec.js
@@ -1,55 +1,68 @@
 describe("TransitLegMassage", function () {
 		
-    describe("Given im the only tourist and the machine is not broken ", function () {
-        it("should return y as the fastest runner", function() {
+    describe("Given you the only tourist and the machine is not broken ", function () {
+        it("should return you as the fastest runner", function() {
             // arrange
             var massage = new legMassage();
             // act
-            var actual = massage.fastestRunner(12,1,0,0,0,0,false);
+            var actual = massage.fastestRunner(12, 1, 0, 0, 0, 0, false);
             // assert
             expect(actual).toBe("y");
             });
     })
 
-    describe("Given its me and another tourist and the machine is not broken ", function () {
+    describe("Given you the only tourist and the machine is broken ", function () {
+        it("should return you as the fastest runner", function() {
+            // arrange
+            var massage = new legMassage();
+            // act
+            var actual = massage.fastestRunner(12, 1, 0, 0, 0, 0, true);
+            // assert
+            expect(actual).toBe("y");
+            });
+    })
+
+    describe("Given there's you and another tourist and the machine is not broken ", function () {
         it("should return t1 as the fastest runner", function() {
             // arrange
             var massage = new legMassage();
             // act
-            var actual = massage.fastestRunner(12,1,10,1,0,0,false);
+            var actual = massage.fastestRunner(12, 1, 9, 1.2, 0, 0, false);
             // assert
             expect(actual).toBe("t1");
             });
     })
 
-   describe("Given its me and another tourist and the machine is broken ", function () {
-        it("should return y as the fastest runner", function() {
+   describe("Given there's you and another tourist and the machine is broken ", function () {
+        it("should return you as the fastest runner", function() {
             // arrange
             var massage = new legMassage();
             // act
-            var actual = massage.fastestRunner(12,1,9,1,0,0,true);
+            var actual = massage.fastestRunner(12, 1, 9, 1.2, 0, 0 ,true);
             // assert
             expect(actual).toBe("y");
             });
     })
 
-    describe("Given its me and 2 other tourists and the machine is not broken ", function () {
+    
+
+    describe("Given there's you and 2 other tourists and the machine is not broken ", function () {
         it("should return t2 as the fastest runner", function() {
             // arrange
             var massage = new legMassage();
             // act
-            var actual = massage.fastestRunner(12,1,9,1,12,2,false);
+            var actual = massage.fastestRunner(12, 1 ,9, 1.2, 15, 2,false);
             // assert
             expect(actual).toBe("t2");
             });
     })
 
-    describe("Given its me and 2 other tourists and the machine is broken ", function () {
+    describe("Given there's you and 2 other tourists and the machine is broken ", function () {
         it("should return t1 as the fastest runner", function() {
             // arrange
             var massage = new legMassage();
             // act
-            var actual = massage.fastestRunner(12,1,13,1,30,2,true);
+            var actual = massage.fastestRunner(12, 1,9, 1.2, 15, 2, true);
             // assert
             expect(actual).toBe("t1");
             });
